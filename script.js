afbcb62341438e1ebff00a80fac3320aedad6f30
Taiyo <taiyoperson@gmail.com>
2022-08-08 10:33:16 +1200

    - Created a working hamburger menu (need to still style with CSS)

---
+++ script.js
@@ -0,0 +1,10 @@
+function toggleMenu() {
+  if (document.getElementById("menu").style.width === "0%")
+  {
+    document.getElementById("menu").style.width = "100%";;
+  }
+  else
+  {
+    document.getElementById("menu").style.width = "0%";
+  }
+}
