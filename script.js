b77d2c6866ee45346bb4545f225c25e5e87ad5b7
Taiyo <taiyoperson@gmail.com>
2022-08-22 17:00:06 +1200

    - Made image hover effect more effecient using JavaScript

--- script.js
+++ script.js
@@ -8,3 +8,12 @@ function toggleMenu() {
     document.getElementById("menu").style.width = "0%";
   }
 }
+
+function changeImage(target) {
+  target.src = target.src.replace("f.jpg", "b.jpg");
+}
+
+function revertImage(target)
+{
+  target.src = target.src.replace("b.jpg", "f.jpg");
+}
