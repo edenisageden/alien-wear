6a6c83edf27dbbafa43dffdebe7688d2f105e570
Taiyo <taiyoperson@gmail.com>
2022-08-28 21:01:38 +1200

    - Added smart navigation bar hide and show

--- script.js
+++ script.js
@@ -1,3 +1,17 @@
+const nav = document.querySelector(".nav");
+let lastScrollY = window.scrollY;
+
+window.addEventListener("scroll", () => {
+  if (lastScrollY < window.scrollY) {
+    nav.classList.add("nav-hidden");
+  }
+  else {
+    nav.classList.remove("nav-hidden");
+  }
+
+  lastScrollY = window.scrollY;
+});
+
 function toggleMenu() {
   if (document.getElementById("menu").style.width === "0%")
   {
